1620cce9477a0142400a40f2e09a67ce58ebaaa6
nekulin <nekulin@mail.ru>
2019-03-23 00:29:50 +0200

    code

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,3 +2,26 @@
 /**
  * Логика плагина
  */
+class AnJwtAuth {
+    private $page;
+    public function __construct(){
+        $this->page = is_multisite() ? 'settings.php' : 'options-general.php';
+        add_filter( 'plugin_action_links', array( $this, 'add_plugin_actions_links' ), 10, 2 );
+        $this->includes();
+    }
+    public function add_plugin_actions_links( $links, $file ) { 
+        if( basename( dirname( $file ) ) == 'an-jwt-auth' ) {
+          $links[] = '<a href="' . esc_url( add_query_arg( array( 'page' => 'an-jwt-auth' ), $this->page ) ) . '">' . __( 'Settings', 'an-jwt-auth' ) . '</a>';
+        }
+        return $links;
+    }
+    public function get_settings_page() {
+        return $this->page;
+    }
+
+    private function includes() {
+        include 'carbon-fields.php';
+    }
+}
+$GLOBALS[ 'AnJwtAuth' ] = new AnJwtAuth();
+
---
+++ core/carbon-fields.php
@@ -0,0 +1,72 @@
+<?php
+use Carbon_Fields\Container;
+use Carbon_Fields\Field; 
+
+if( !defined('AN_JWT_AUTH') || AN_JWT_AUTH!==true ) die();
+
+add_action( 'after_setup_theme', 'an_jwt_auth_load_carbon_fields' );
+function an_jwt_auth_load_carbon_fields() {
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+
+function an_jwt_auth_set_carbon_fields(){
+        global $AnJwtAuth;
+        Container::make( 'theme_options', __( 'JWT auth settings', 'an-jwt-auth' ) )
+        ->set_page_parent( $AnJwtAuth->get_settings_page() ) 
+        ->set_page_file( 'an-jwt-auth' )
+        ->set_page_menu_title(  __( 'JWT AUTH', 'an-jwt-auth' ) )
+        ->add_fields( array(
+            Field::make( 'select', 'jwt_init', __( 'JWT switch?', 'an-jwt-auth' ) )
+            ->set_options( array(
+                '1' => __( 'Yes', 'an-jwt-auth' ),
+                '0' => __( 'No', 'an-jwt-auth' )
+            ) ),
+			Field::make( 'text', 'jwt_nbf', __( 'Access token lifetime in seconds', 'an-jwt-auth' ) )
+            ->set_attribute( 'type', 'number' )->set_attribute( 'step', '10' )
+            ->set_default_value( 3600 )
+            ->set_conditional_logic( array(
+                'relation' => 'AND', 
+                array(
+                    'field' => 'jwt_init',
+                    'value' => '1',
+                    'compare' => '=', 
+                )
+            ) ),
+			Field::make( 'text', 'jwt_nbf_session', __( 'The lifetime of the "refresh token" in seconds', 'an-jwt-auth' ) )
+            ->set_attribute( 'type', 'number' )->set_attribute( 'step', '10' )
+            ->set_default_value( 2678400 )
+            ->set_conditional_logic( array(
+                'relation' => 'AND', 
+                array(
+                    'field' => 'jwt_init',
+                    'value' => '1',
+                    'compare' => '=', 
+                )
+            ) ),
+            Field::make( 'text', 'jwt_key', __( 'Secret JWT Key (Used to encrypt JWT)', 'an-jwt-auth' ) )
+            ->set_default_value( 'USED_TO_ENCRYPT_JWTKEY' )
+            ->set_conditional_logic( array(
+                'relation' => 'AND', 
+                array(
+                    'field' => 'jwt_init',
+                    'value' => '1',
+                    'compare' => '=', 
+                )
+            ) ),
+            Field::make( 'select', 'jwt_cors', __( 'Use CORs?', 'an-jwt-auth' ) )
+            ->set_help_text( 'Cross-origin resource sharing (<a href="https://en.wikipedia.org/wiki/Cross-origin_resource_sharing">CORS</a>; с англ. — «совместное использование ресурсов между разными источниками») — технология современных браузеров, которая позволяет предоставить веб-странице доступ к ресурсам другого домена.' )
+            ->set_options( array(
+                '1' => __( 'Yes', 'an-jwt-auth' ),
+                '0' => __( 'No', 'an-jwt-auth' )
+            ) ) 
+            ->set_conditional_logic( array(
+                'relation' => 'AND', 
+                array(
+                    'field' => 'jwt_init',
+                    'value' => '1',
+                    'compare' => '=', 
+                )
+            ) ),
+        ) );
+}
+add_action( 'carbon_fields_register_fields', 'an_jwt_auth_set_carbon_fields' );
